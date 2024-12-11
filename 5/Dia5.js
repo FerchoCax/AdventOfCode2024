d4a3420c9befd367098a55a7579a8ede9714201c
FerchoCax <ferchonando26@gmail.com>
2024-12-10 23:33:25 -0600

    Completando dia 5

--- 5/Dia5.js
+++ 5/Dia5.js
@@ -13,11 +13,80 @@ try {
     return e[0].split(',').map(v => {return Number.parseInt(v)})
   }).filter(e => e.length > 1)
 
+  let count = 0
+  let unorderedCount = 0
   for (const linea of values) {
-    console.log(linea)
+    let valid = validLine(linea)
+    if(valid){
+      count +=  getCenterValue(linea)
+    }else{
+      validOrderLine(linea)
+    }
   }
 
+  //Parte 1
+  console.log(count)
+  //Parte 2
+  console.log(unorderedCount)
+
+
+  function validatePosition(first, second){
+    if(first == second){
+      return true;
+    }
+    let vals = rules.filter(e => e[0] == first && e[1] == second).length > 0
+    return vals
+  }
+  function getCenterValue(linea){
+    let medium = 0
+    if(linea.length%2 == 0){
+      medium = (linea.length /2)-1;
+      return linea[medium] + linea[medium+1]
+    }else{
+      medium = ((linea.length -1) /2)
+      return linea[medium]
+    }
+  }
+
+  function validLine(linea){
+    let valid = true;
+    for (let i = 0; i < linea.length; i++) {
+      for(let j = i; j< linea.length; j++){
+        valid = validatePosition(linea[i], linea[j])
+        if(!valid){
+          break;
+        }
+      } 
+      if(!valid){
+        break;
+      }
+    }
+    return valid
+  }
+
+  function validOrderLine(linea){
+    let valid = true;
+    for (let i = 0; i < linea.length; i++) {
+      for(let j = i; j< linea.length; j++){
+        valid = validatePosition(linea[i], linea[j])
+        if(!valid){
+          tempLine = [...linea];
+          tempLine[i] = linea[j];
+          tempLine[j] = linea[i]
+          if(validOrderLine(tempLine)){
+            unorderedCount += getCenterValue(tempLine)
+          }
+          break;
+        }
+      } 
+      if(!valid){
+        break;
+      }
+    }
+    return valid
+  }
 
 } catch (err) {
     console.log(err)
 }
+
