06a5b34de90ced09db090358937cbf63813bddaf
FerchoCax <ferchonando26@gmail.com>
2024-12-16 16:00:04 -0600

    Prueba 1 dia 7 parte2

--- 7/Dia7.js
+++ 7/Dia7.js
@@ -3,20 +3,33 @@ const filePath = './input.txt';
 try {
   const fileContent = fs.readFileSync(filePath, 'utf-8');
   let values = fileContent.split('\n').map(e => e.replace('\r', '').split(':'))
-  let total = 0
-  let activeWorkers = 0
+  let totalPart1 = 0
+  let totalPart2 = 0
   for (const pair of values) {
     let responce = Number.parseInt(pair[0])
-    console.log(pair)
+    // console.log(pair)
     let vals = pair[1].trim().split(' ').map(function(item) {
       return parseInt(item, 10);
     })
     if(findCalibration(responce, vals)){
-      total += responce
+      totalPart1 += responce
     }
-    // break
   }
-  console.log(total)
+
+  for (const pair of values) {
+    let responce = Number.parseInt(pair[0])
+    // console.log(pair)
+    let vals = pair[1].trim().split(' ').map(function(item) {
+      return parseInt(item, 10);
+    })
+    if(findCalibration(responce, vals) || findCalibrationWhitConcatenation(responce, vals)){
+      console.log(pair)
+      totalPart2 += responce
+    }
+  }
+
+  console.log(totalPart1)
+  console.log(totalPart2)
 } catch (err) {
     console.log(err)
 }
@@ -35,4 +48,25 @@ function findCalibration(responce, vals) {
     }
   }
   return false;
+}
+
+function findCalibrationWhitConcatenation(responce, vals){
+  const results = rowPermutations(vals);
+  for (const array of results) {
+    if(findCalibration(responce, array)){
+      return true
+    }
+  }
+  return false
+}
+
+function rowPermutations(vals) {
+  let results = []
+  for (let i = 0; i < vals.length - 1; i++) {
+    const newArr = [...vals];
+    const newValue = Number.parseInt(newArr[i].toString()+ newArr[i + 1].toString());
+    newArr.splice(i, 2, newValue);
+    results.push(newArr);
+  }
+  return results
 }
