29341868dc2f96c919a9897e002e3ff64a550270
FerchoCax <ferchonando26@gmail.com>
2024-12-20 09:30:35 -0600

    Terminando dia 7

--- 7/Dia7.js
+++ 7/Dia7.js
@@ -22,11 +22,13 @@ try {
     let vals = pair[1].trim().split(' ').map(function(item) {
       return parseInt(item, 10);
     })
-    if(findCalibration(responce, vals) || findCalibrationWhitConcatenation(responce, vals)){
-      console.log(pair)
+    if(findCalibrationWhitConcatenation(responce, vals)){
+      // console.log(pair)
       totalPart2 += responce
     }
   }
+//32565384255325
+//32565397601168
 
   console.log(totalPart1)
   console.log(totalPart2)
@@ -51,15 +53,57 @@ function findCalibration(responce, vals) {
 }
 
 function findCalibrationWhitConcatenation(responce, vals){
-  const results = rowPermutations(vals);
-  for (const array of results) {
-    if(findCalibration(responce, array)){
-      return true
+  const operatios = vals.length - 1; 
+  const trits = 3 ** operatios;      
+  for (let count = 0; count < trits; count++) {
+    let total = vals[0];
+    let currentCount = count; 
+    for (let i = 0; i < operatios; i++) {
+      const operation = currentCount % 3; 
+      currentCount = Math.floor(currentCount / 3); 
+      if (operation === 0) {
+        total += vals[i + 1]; 
+      } else if (operation === 1) {
+        total *= vals[i + 1]; 
+      } else if (operation === 2) {
+        total = Number.parseInt(total.toString()+vals[i + 1].toString()) ; 
+      }
+    }
+    if (total === responce) {
+      return true; 
     }
   }
-  return false
-}
+  return false; 
+  // const operatios = vals.length - 1;
+  // const factorial = 1 << operatios; 
+  // let position = 1
+  // let total = 0;
 
+  // for (let count = 0; count < factorial; count++) {
+  //     for (let j = 0; j < operatios; j++) {
+  //     position = j
+  //     total = vals[0]
+  //     for (let i = 0; i < operatios; i++) {
+  //       if(position == i){
+          
+  //         total = Number.parseInt(total.toString() + vals[i+1].toString())
+  //       }else{
+  //         const isMultiply = (count & (1 << i)) !== 0; 
+  //         total = isMultiply ? total * vals[i + 1] : total + vals[i + 1];
+  //         if(i+1 === vals.length-1 && total === responce){
+  //           return true
+  //         }
+  //       }
+  //   }
+  //   }
+   
+  //   // console.log(total)
+  //   if (total === responce) {
+  //     return true;
+  //   }
+  // }
+  // return false;
+}
 function rowPermutations(vals) {
   let results = []
   for (let i = 0; i < vals.length - 1; i++) {
